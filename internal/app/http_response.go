2e5053aa26c7a9c7ee140ad40ae50a3037ce4b02
renchili <33677006+renchili@users.noreply.github.com>
2026-06-15 15:31:12 +0800

    Add HTTP helpers

---
+++ internal/app/http_response.go
@@ -0,0 +1,13 @@
+package app
+
+import (
+    "github.com/labstack/echo/v4"
+)
+
+type Payload struct {
+    Data interface{} `json:"data,omitempty"`
+}
+
+func sendJSON(c echo.Context, code int, value interface{}) error {
+    return c.JSON(code, Payload{Data: value})
+}
