6aed445b79dfa001eb6a0649aa70edd7356fdb88
renchi.li <renchi.li@bytedance.com>
2026-06-15 23:18:13 +0800

    Add compare API coverage

---
+++ API_tests/test_compare_versions.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -u -o pipefail
+. API_tests/lib.sh
+FAIL=0
+
+: "${EDITOR_TOKEN:?set EDITOR_TOKEN}"
+
+DOC_ID=$(cat "${DOC_ID_FILE:-/tmp/ironpage_doc_id.out}")
+
+code=$(auth_get "$EDITOR_TOKEN" "/api/documents/$DOC_ID/versions")
+expect_code "list versions before compare" 200 "$code" || FAIL=$((FAIL+1))
+
+LEFT=$(json_field data[0].id)
+RIGHT="$LEFT"
+
+code=$(auth_post_json "$EDITOR_TOKEN" /api/documents/compare "{\"left_version_id\":\"$LEFT\",\"right_version_id\":\"$RIGHT\"}")
+expect_code "compare identical versions" 200 "$code" || FAIL=$((FAIL+1))
+
+code=$(auth_post_json "$EDITOR_TOKEN" /api/documents/compare '{"left_version_id":"missing-left","right_version_id":"missing-right"}')
+expect_code "compare missing versions rejected" 404 "$code" || FAIL=$((FAIL+1))
+
+exit "$FAIL"
