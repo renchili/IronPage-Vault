bbf58c2774804b17c09880483eea1a043c2f3ac0
renchi.li <renchi.li@bytedance.com>
2026-06-17 02:51:00 +0800

    Allow CI Go tests to resolve modules

--- run_tests.sh
+++ run_tests.sh
@@ -5,7 +5,7 @@ if [ -x unit_tests/test_rules.sh ]; then
   unit_tests/test_rules.sh
 fi
 
-go test ./...
+go test -mod=mod ./...
 
 if [ -x API_tests/test_api_flow.sh ]; then
   API_tests/test_api_flow.sh
