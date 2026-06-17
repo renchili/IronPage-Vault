7da83f32d7aa9936e687535e01260e795724d193
renchili <33677006+renchili@users.noreply.github.com>
2026-06-17 21:55:11 +0800

    Run document review subtests through bash

--- API_tests/test_documents_review.sh
+++ API_tests/test_documents_review.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -u -o pipefail
-API_tests/test_document_upload.sh
-API_tests/test_workflow_transitions.sh
+bash API_tests/test_document_upload.sh
+bash API_tests/test_workflow_transitions.sh
