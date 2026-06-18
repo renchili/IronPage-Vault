61232edd0751da7b2968a3c0a4b8465223f9b37c
renchili <33677006+renchili@users.noreply.github.com>
2026-06-19 01:14:29 +0800

    Use project Swagger generation flow inside builder

--- scripts/build_server_in_container.sh
+++ scripts/build_server_in_container.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env sh
 set -eu
 
+# The application blank-imports the generated Swagger package. Create a
+# temporary declaration first so module resolution can complete inside this
+# image, then regenerate the package with the same project script used by CI.
 mkdir -p docs/swagger
 printf 'package swagger\n' > docs/swagger/docs.go
 go mod tidy
-go run github.com/swaggo/swag/cmd/swag@v1.16.4 init -g cmd/server/main.go -o docs/swagger --parseInternal --parseDependency
+go install github.com/swaggo/swag/cmd/swag@v1.16.4
+bash scripts/generate_swagger.sh
 go build -mod=mod -o /out/ironpage ./cmd/server
