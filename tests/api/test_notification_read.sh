24ba3a3caed50e4d400d84f505814bab3587a427
renchili <33677006+renchili@users.noreply.github.com>
2026-07-19 01:33:15 +0800

    use canonical API helper path

--- tests/api/test_notification_read.sh
+++ tests/api/test_notification_read.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -u -o pipefail
-. API_tests/lib.sh
+. tests/api/lib.sh
 FAIL=0
 : "${EDITOR_TOKEN:?set EDITOR_TOKEN}"
 code=$(auth_post_json "$EDITOR_TOKEN" "/api/notifications/not_missing/read" '{}')
