3d7ae0bf9f93ac0a349e21b535939ec765255af9
renchili <33677006+renchili@users.noreply.github.com>
2026-07-19 01:39:45 +0800

    use canonical document review paths

--- tests/api/test_documents_review.sh
+++ tests/api/test_documents_review.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -u -o pipefail
-bash API_tests/test_document_upload.sh
-bash API_tests/test_workflow_transitions.sh
+bash tests/api/test_document_upload.sh
+bash tests/api/test_workflow_transitions.sh
